349fb56c853f44aa54a8fd4f6edf7e4ccdb4a73c
Mats Julian Olsen <mats@plysjbyen.net>
2015-11-01 10:40:49 +0100

    Moved source into src and tests to test

---
+++ src/transform.js
@@ -0,0 +1,51 @@
+'use strict';
+import {map, reduce, keys, union, get, set, includes, omit, pick,
+        isFunction, isArray, isString, isPlainObject} from 'lodash';
+
+export default function (data, transforms, exclude) {
+ 
+  if (!transforms && !exclude) 
+    return data;
+  if (!transforms && exclude)
+    return drop(data, exclude);
+  
+  if (isString(transforms))
+    return select(data, [transforms]);
+
+  if (isArray(transforms))
+    return select(data, transforms);
+
+  if (isString(exclude))
+    exclude = [exclude];
+
+  return map(data, function (element) {
+    return reduce(element, function (acc, val, key) { 
+      var t = get(transforms, key);
+      if (isFunction(t)) {
+        let res = t(val);
+        acc[res.key || key] = res.value || res;
+      } else if (isString(t)) {
+        acc[t] = val;
+      } else if (includes(exclude, key) || exclude === true) {
+        ;
+      } else {
+        acc[key] = val;
+      }
+      return acc;
+    }, {});
+  });
+}
+
+function select(data, includeKeys) {
+  return map(data, function (obj) {
+    return reduce(includeKeys, function (acc, include) {
+      acc = set(acc, include, get(obj, include));
+      return acc;
+    }, {});
+  });
+}
+
+function drop(data, excludeKeys) {
+  return map(data, (obj) => omit(obj, excludeKeys));
+}
+
