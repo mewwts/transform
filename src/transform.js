96d3daf627eb7b87e0432dfb3ffdc669a76699a7
Mats Julian Olsen <mats@plysjbyen.net>
2015-11-02 19:45:17 +0100

    New API!

--- src/transform.js
+++ src/transform.js
@@ -2,38 +2,40 @@
 import {map, reduce, keys, union, get, set, includes, omit, pick,
         isFunction, isArray, isString, isPlainObject} from 'lodash';
 
-module.exports = function (data, transforms, exclude) {
+module.exports = function (transforms, exclude) {
  
-  if (!transforms && !exclude) 
-    return data;
-  if (!transforms && exclude)
-    return drop(data, exclude);
-  
-  if (isString(transforms))
-    return select(data, [transforms]);
+  return function (data) {
+    if (!transforms && !exclude) 
+      return data;
+    if (!transforms && exclude)
+      return drop(data, exclude);
+    
+    if (isString(transforms))
+      return select(data, [transforms]);
 
-  if (isArray(transforms))
-    return select(data, transforms);
+    if (isArray(transforms))
+      return select(data, transforms);
 
-  if (isString(exclude))
-    exclude = [exclude];
+    if (isString(exclude))
+      exclude = [exclude];
 
-  return map(data, function (element) {
-    return reduce(element, function (acc, val, key) { 
-      var t = get(transforms, key);
-      if (isFunction(t)) {
-        let res = t(val);
-        acc[res.key || key] = res.value || res;
-      } else if (isString(t)) {
-        acc[t] = val;
-      } else if (includes(exclude, key) || exclude === true) {
-        ;
-      } else {
-        acc[key] = val;
-      }
-      return acc;
-    }, {});
-  });
+    return map(data, function (element) {
+      return reduce(element, function (acc, val, key) { 
+        var t = get(transforms, key);
+        if (isFunction(t)) {
+          let res = t(val);
+          acc[res.key || key] = res.value || res;
+        } else if (isString(t)) {
+          acc[t] = val;
+        } else if (includes(exclude, key) || exclude === true) {
+          ;
+        } else {
+          acc[key] = val;
+        }
+        return acc;
+      }, {});
+    });
+  }
 }
 
 function select(data, includeKeys) {
--- test/test-transform.js
+++ test/test-transform.js
@@ -25,66 +25,65 @@ var obj2 = {
 var data = [obj1, obj2];
 
 test('Transform should return the data object when no columns are specified', function (t) {
-  var result = transform(data); // last parameters is falsy
+  var result = transform()(data); // last parameters is falsy
   t.equal(result, data);
   t.end();
 });
 
 test('Transform should return a clone of the data object when all columns are specified as an array', function (t) {
-  var result = transform(data, ['id', 'val1', 'val2', 'val3']);
+  var result = transform(['id', 'val1', 'val2', 'val3'])(data);
   t.deepEqual(result, data);
   t.end();
 });
 
 test('Transform should be able to get and set nested objects through a list', function (t) {
-  var key = 'val3.val3';
-  var result = transform([obj1], [key]);
+  var result = transform('val3.val3')([obj1]);
   t.deepEqual(result, [{val3: {val3: 'coolness'}}]);
   t.end();
 });
 
 test('Transform should omit and include when arguments are arrays', function (t) {
-  var result = transform([obj1], ['val1']);
+  var result = transform(['val1'])([obj1]);
   t.deepEqual(result, [_.pick(obj1, 'val1')]);
   t.end();
 });
 
 
 test('Transform should omit fields when they are specified in the third arguments', function (t) {
-  var result = transform([obj1], null, ['val3']);
+  var result = transform(null, 'val3')([obj1]);
   t.deepEqual(result, [_.omit(obj1, ['val3'])]);
   t.end();
 });
 
 test('Transform should be able to get and set nested objects through an array', function (t) {
-  var result = transform(data, ['val3.val3']);
+  var result = transform(['val3.val3'])(data);
   t.deepEqual(result, [{val3: {val3: 'coolness'}}, {val3: {val3: 'super'}}]);
   t.end();
 });
 
 test('Transform should return a clone of the data object when columns are specified without a transform', function (t) {
-  var result = transform(data, {
+  var result = transform({
     id: {},
     val1: {},
     val2: {},
     val3: {}
-  });
+  })(data);
   t.deepEqual(result, data);
   t.end();
 });
 
 test('Transform should transform a value when given a function and exclude others when last param is true', function (t) {
-  var result = transform(data, {
+  var result = transform({
     val1: (val) => val + 1 
-  }, true);
+  }, true)(data);
   t.deepEqual(result, [{val1: obj1.val1 + 1}, {val1: obj2.val1 + 1}]);
   t.end();
 });
 
 test('Transform should include other fields when last param is falsy ', function (t) {
-  var result = transform(data, {
+  var result = transform({
     val1: (val) => val + 1 
-  });
+  })(data);
   var clone = _.cloneDeep(data);
   clone[0].val1 += 1;
   clone[1].val1 += 1;
@@ -93,9 +92,9 @@ test('Transform should include other fields when last param is falsy ', function
 });
 
 test('Transform should exclude some fields', function (t) {
-  var result = transform(data, {
+  var result = transform({
     val1: (val) => val + 1 
-  }, 'val3');
+  }, 'val3')(data);
 
   var clone = _.cloneDeep(data);
   clone[0].val1 += 1;
@@ -108,36 +107,24 @@ test('Transform should exclude some fields', function (t) {
 
 test('Transform should include all fields but ommited even when transforms are given', function (t) {
   var d = [{'x': 1, 'y': 2, 'z': 3}, {'x': 2, 'y': 3, 'z': 4}]
-  var result = transform(d, {x: {}}, ['z']);
+  var result = transform({x: {}}, ['z'])(d);
   t.deepEqual(result, [{'x': 1, 'y': 2}, {'x': 2, 'y': 3}]);
   t.end();
 });
 
 test('Transform should allow setting a new key', function (t) {
   var d = [{'x': 1, 'y': 2}]
-  var result = transform(d, {x: 'y', y: 'x'});
+  var result = transform({x: 'y', y: 'x'})(d);
   t.deepEqual(result, [{'y': 1, 'x': 2}]);
   t.end();
 });
 
 test('Transform should allow setting a new key while transforming', function (t) {
   var d = [{'x': 1, 'y': 2}]
-  var result = transform(d, {
+  var result = transform({
     x: (val) => ({key: 'y', value: val}),
     y: (val) => ({key: 'x', value: val})
-  });
+  })(d);
   t.deepEqual(result, [{'y': 1, 'x': 2}]);
-  t.end();
-});
-
-test('Transform should allow transforming nested fields', function (t) {
-  var d = [{x: {y: 2015}}, {x: {y: 2016}}];
-  var result = transform(d, {
-    'x.y': (val) => val + 1
-  });
-  var c = _.cloneDeep(d);
-  c[0].x.y += 1;
-  c[1].x.y += 1;
-  t.deepEqual(result, c);
   t.end();
 });
