1590ec1b93f741c3a5364a9f18a4d56480663b3b
Mats Julian Olsen <mats@plysjbyen.net>
2015-11-02 20:01:00 +0100

    Well this needs to be included

--- transform.transpiled.js
+++ transform.transpiled.js
@@ -2,33 +2,35 @@
 
 var _lodash = require('lodash');
 
-module.exports = function (data, transforms, exclude) {
+module.exports = function (transforms, exclude) {
 
-  if (!transforms && !exclude) return data;
-  if (!transforms && exclude) return drop(data, exclude);
+  return function (data) {
+    if (!transforms && !exclude) return data;
+    if (!transforms && exclude) return drop(data, exclude);
 
-  if ((0, _lodash.isString)(transforms)) return select(data, [transforms]);
+    if ((0, _lodash.isString)(transforms)) return select(data, [transforms]);
 
-  if ((0, _lodash.isArray)(transforms)) return select(data, transforms);
+    if ((0, _lodash.isArray)(transforms)) return select(data, transforms);
 
-  if ((0, _lodash.isString)(exclude)) exclude = [exclude];
+    if ((0, _lodash.isString)(exclude)) exclude = [exclude];
 
-  return (0, _lodash.map)(data, function (element) {
-    return (0, _lodash.reduce)(element, function (acc, val, key) {
-      var t = (0, _lodash.get)(transforms, key);
-      if ((0, _lodash.isFunction)(t)) {
-        var res = t(val);
-        acc[res.key || key] = res.value || res;
-      } else if ((0, _lodash.isString)(t)) {
-        acc[t] = val;
-      } else if ((0, _lodash.includes)(exclude, key) || exclude === true) {
-        ;
-      } else {
-        acc[key] = val;
-      }
-      return acc;
-    }, {});
-  });
+    return (0, _lodash.map)(data, function (element) {
+      return (0, _lodash.reduce)(element, function (acc, val, key) {
+        var t = (0, _lodash.get)(transforms, key);
+        if ((0, _lodash.isFunction)(t)) {
+          var res = t(val);
+          acc[res.key || key] = res.value || res;
+        } else if ((0, _lodash.isString)(t)) {
+          acc[t] = val;
+        } else if ((0, _lodash.includes)(exclude, key) || exclude === true) {
+          ;
+        } else {
+          acc[key] = val;
+        }
+        return acc;
+      }, {});
+    });
+  };
 };
 
 function select(data, includeKeys) {
